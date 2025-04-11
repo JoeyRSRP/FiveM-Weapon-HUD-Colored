e4d15d0c4b907adcf9534d21b9569787f5f01dd9
Joey <jkhoshaba315@gmail.com>
2025-04-10 20:58:40 -0700

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Riverside State Roleplay' -- https://discord.gg/riversidesrp
+description 'Weapon HUD Colored' -- https://github.com/JoeyRSRP/FiveM-Weapon-HUD-Colored
+
+client_script 'cl_weaponhud.lua'
